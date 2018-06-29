d86235528f4c02b528d76b38fc469d5aced45757
avasilevich <andrey.vasilevich@pressindex.ru>
2018-06-29 21:31:52 +0700

    Initial commit

---
+++ ticket-service/src/main/kotlin/ru/avp/ticket/TicketServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.avp.ticket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TicketServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<TicketServiceApplication>(*args)
+}
